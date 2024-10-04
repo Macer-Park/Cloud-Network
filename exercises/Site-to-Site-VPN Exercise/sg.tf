7aeedbb525734fec6c300f2977c4f05b0f59e31e
Macer-Park <pkjm1705@gmail.com>
2024-10-04 15:39:41 +0900

    Edit Security Groups for Egress Rules

--- exercises/Site-to-Site-VPN Exercise/sg.tf
+++ exercises/Site-to-Site-VPN Exercise/sg.tf
@@ -56,6 +56,13 @@ resource "aws_security_group" "SG2" {
     protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "SG3" {
@@ -77,6 +84,13 @@ resource "aws_security_group" "SG3" {
     from_port   = -1
     to_port     = -1
     protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
